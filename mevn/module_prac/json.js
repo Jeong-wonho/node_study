813914de83aaedbbdbc57383988e904bd9a8d806
Jeong-wonho <kallzzang@naver.com>
2023-06-06 10:18:18 +0900

    lazy_loading 연습

---
+++ mevn/module_prac/json.js
@@ -0,0 +1,5 @@
+let a ='{"이름":"큰돌"}'
+console.log(typeof a)
+console.log(a.이름)
+a = JSON.parse(a)
+console.log(typeof a)
---
+++ mevn/module_prac/simple_json.js
@@ -0,0 +1,11 @@
+const obj = {
+    "이름":"큰돌",
+    "나이":"27",
+    "나이2":undefined
+}
+
+console.log(obj.이름);
+const name = "이름"
+console.log(obj[name]);
+console.log(obj['이름']);
+console.log(obj['나이2']);
